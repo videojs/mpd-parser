b32a038e31e2aa8437fd88deea37dd1b89d66393
brandonocasey <brandonocasey@gmail.com>
2019-08-30 15:12:12 -0400

    cr change exports

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import { version } from '../package.json';
-import { toM3u8 as _toM3u8} from './toM3u8';
-import { toPlaylists as _toPlaylists} from './toPlaylists';
-import { inheritAttributes as _inheritAttributes} from './inheritAttributes';
-import { stringToMpdXml as _stringToMpdXml} from './stringToMpdXml';
+import { toM3u8 } from './toM3u8';
+import { toPlaylists } from './toPlaylists';
+import { inheritAttributes } from './inheritAttributes';
+import { stringToMpdXml } from './stringToMpdXml';
 import { parseUTCTimingScheme } from './parseUTCTimingScheme';
 
-export const VERSION = version;
+const VERSION = version;
 
-export const parse = (manifestString, options = {}) =>
-  _toM3u8(_toPlaylists(_inheritAttributes(_stringToMpdXml(manifestString), options)), options.sidxMapping);
+const parse = (manifestString, options = {}) =>
+  toM3u8(toPlaylists(inheritAttributes(stringToMpdXml(manifestString), options)), options.sidxMapping);
 
 /**
  * Parses the manifest for a UTCTiming node, returning the nodes attributes if found
@@ -18,10 +18,15 @@ export const parse = (manifestString, options = {}) =>
  * @return {Object|null}
  *         Attributes of UTCTiming node specified in the manifest. Null if none found
  */
-export const parseUTCTiming = (manifestString) =>
-  parseUTCTimingScheme(_stringToMpdXml(manifestString));
+const parseUTCTiming = (manifestString) =>
+  parseUTCTimingScheme(stringToMpdXml(manifestString));
 
-export const stringToMpdXml = _stringToMpdXml;
-export const inheritAttributes = _inheritAttributes;
-export const toPlaylists = _toPlaylists;
-export const toM3u8 = _toM3u8;
+export {
+  VERSION,
+  parse,
+  parseUTCTiming,
+  stringToMpdXml,
+  inheritAttributes,
+  toPlaylists,
+  toM3u8
+};
