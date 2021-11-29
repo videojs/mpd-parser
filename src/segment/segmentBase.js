50eb4349a15d4af6254f6d287a950254265416dc
Brandon Casey <2381475+brandonocasey@users.noreply.github.com>
2021-11-29 11:52:28 -0500

    feat: support BigInt values (#135)

--- src/segment/segmentBase.js
+++ src/segment/segmentBase.js
@@ -1,6 +1,7 @@
 import errors from '../errors';
 import urlTypeConverter from './urlType';
 import { parseByDuration } from './durationTimeParser';
+import window from 'global/window';
 
 /**
  * Translates SegmentBase into a set of segments.
@@ -84,7 +85,14 @@ export const addSidxSegmentsToPlaylist = (playlist, sidx, baseUrl) => {
   const type = playlist.endList ? 'static' : 'dynamic';
 
   // firstOffset is the offset from the end of the sidx box
-  let startIndex = sidxEnd + sidx.firstOffset;
+  let startIndex;
+
+  // eslint-disable-next-line
+  if (typeof sidx.firstOffset === 'bigint') {
+    startIndex = window.BigInt(sidxEnd) + sidx.firstOffset;
+  } else {
+    startIndex = sidxEnd + sidx.firstOffset;
+  }
 
   for (let i = 0; i < mediaReferences.length; i++) {
     const reference = sidx.references[i];
@@ -94,7 +102,14 @@ export const addSidxSegmentsToPlaylist = (playlist, sidx, baseUrl) => {
     // this will be converted to seconds when generating segments
     const duration = reference.subsegmentDuration;
     // should be an inclusive range
-    const endIndex = startIndex + size - 1;
+    let endIndex;
+
+    // eslint-disable-next-line
+    if (typeof startIndex === 'bigint') {
+      endIndex = startIndex + window.BigInt(size) - window.BigInt(1);
+    } else {
+      endIndex = startIndex + size - 1;
+    }
     const indexRange = `${startIndex}-${endIndex}`;
 
     const attributes = {
@@ -116,7 +131,11 @@ export const addSidxSegmentsToPlaylist = (playlist, sidx, baseUrl) => {
     }
 
     segments.push(segment);
-    startIndex += size;
+    if (typeof startIndex === 'bigint') {
+      startIndex += window.BigInt(size);
+    } else {
+      startIndex += size;
+    }
   }
 
   playlist.segments = segments;
